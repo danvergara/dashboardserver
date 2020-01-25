12dea8c0ede38c01fa7631a4a002eadf3445fac5
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-01-25 12:02:34 -0600

    feat(endpoints): added /currency-exchange endpoint
    This new endpoint returns the  USD-MXN convertion

--- main.go
+++ main.go
@@ -4,15 +4,34 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/danvergara/newsapigo"
 	"github.com/gin-gonic/gin"
+	"github.com/mattevans/dinero"
 )
 
 func main() {
 	r := gin.Default()
 
 	APIKey := os.Getenv("NEWSAPI_KEY")
+
+	r.GET("/currency-exchange", func(c *gin.Context) {
+		currencyClient := dinero.NewClient(
+			os.Getenv("OPEN_EXCHANGE_APP_ID"),
+			"USD",
+			20*time.Minute,
+		)
+
+		rsp, err := currencyClient.Rates.Get("MXN")
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"dolar-peso": rsp})
+	})
 
 	r.GET("/top-news", func(c *gin.Context) {
 		client := newsapigo.NewsClient{
