e03b48d41a43e0be080ca34ca0b212effd33776a
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-08-17 23:16:00 -0500

    docs(jobs-handler): add the swagger documentation for the jobs handler

--- pkg/handlers/jobs/jobs.go
+++ pkg/handlers/jobs/jobs.go
@@ -13,6 +13,33 @@ type jobsResponse struct {
 	Jobs []githubjobs.Job `json:"jobs"`
 }
 
+//lint:file-ignore U1000 Ignore all unused code, it's generated
+
+// A list of jobs for software engineers  from the Github Jobs API
+// swagger:response jobsResponse
+type jobsResponseWrapper struct {
+	// in: body
+	Body jobsResponse
+}
+
+// The necessary params to filter the list of jobs
+// swagger:parameters jobs
+type jobsParameters struct {
+	// The desired job description, mostly used to specify the programming language
+	// in: query
+	Description string `json:"description"`
+}
+
+// swagger:route GET /v1/jobs jobs
+//
+// Returns a jobs list for software engineers from Github Jobs API
+//
+// Produces:
+// - application/json
+//
+// Responses:
+//	200: jobsResponse
+
 // Get returns a list of job positions for software engineers from Github Jobs API
 func Get(app *application.Application) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
